3822f1910e0aa74a29b6e9eeb9534254e10dbea5
Luciana Rocha <lucianajrocha97@gmail.com>
2024-02-19 12:24:47 -0300

    implementando blog-pessoal-react

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,27 @@ import Navbar from './componentes/navbar/navbar';
 import Footer from './componentes/footer/footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
-);
+  );
 }
 export default App;
