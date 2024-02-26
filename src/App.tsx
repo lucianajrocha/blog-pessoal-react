2a9258e9b0e866259b01a5f3cece16e6eeb62276
Luciana Rocha <lucianajrocha97@gmail.com>
2024-02-26 18:22:46 -0300

    put, post e delete de postagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import Home from './paginas/home/Home';
-import Navbar from './componentes/navbar/navbar';
-import Footer from './componentes/footer/footer';
+import './App.css';
+
+import Navbar from './componentes/navbar/Navbar';
+import Footer from './componentes/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
+import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './componentes/temas/listaTemas/ListaTemas';
+import FormularioTema from './componentes/temas/formularioTema/FormularioTema';
+import DeletarTema from './componentes/temas/deletarTema/DeletarTema';
+import ListaPostagens from './componentes/postagens/listaPostagens/ListaPostagens';
+import FormularioPostagem from './componentes/postagens/formularioPostagem/FormularioPostagem';
+import DeletarPostagem from './componentes/postagens/deletarPostagem/DeletarPostagem';
+
 
 function App() {
   return (
@@ -19,6 +28,14 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
